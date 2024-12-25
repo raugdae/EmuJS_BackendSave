b9a27523ae4f3655bc5f5406965d9d2a11964195
raugdae <raugdae@gmail.com>
2024-12-25 22:36:49 +0100

    well... another one

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const listeningPort = process.env.API_LISTENING_PORT;
 
 
 const app = express();
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
  }));
  
+
+
+app.use(express.json({limit:'100mb'}));
+app.use(express.urlencoded({limit:'100mb',extended :true, parameterlimit:1000000}));
+
  // Additional headers for broader access
  app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -27,10 +33,6 @@ app.use(cors({
 
 
 
-
-
-app.use(express.json({limit:'100mb'}));
-app.use(express.urlencoded({limit:'100mb',extended :true, parameterlimit:1000000}));
 app.use('/api/savemanagement', saveRoutes);
 
 app.listen(listeningPort,() => console.log('API running on port : '+listeningPort))
