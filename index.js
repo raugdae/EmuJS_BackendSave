719cf749f0f94dd87787eada9a46febbe5100260
raugdae <raugdae@gmail.com>
2024-12-25 21:22:59 +0100

    Update CORS

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const pool = require('./db');
+const authRoutes = require('./Routes/auth');
+const authenticateToken = require('./Routes/authMiddleware');
+
+
 const app = express();
 
 const saveRoutes = require('./Routes/saveRoutes');
 
 const listeningPort = process.env.API_LISTENING_PORT;
 
-app.use(cors());
+app.use(cors({origin : *,
+    credentials:true,
+    methods:['GET','POST'],
+    allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json({limit:'100mb'}));
 app.use(express.urlencoded({limit:'100mb',extended :true, parameterlimit:1000000}));
 app.use('/api/savemanagement', saveRoutes);
