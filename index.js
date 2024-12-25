2c320079d7125a82e65ff544ebd99217dd3d11b6
raugdae <raugdae@gmail.com>
2024-12-25 21:47:50 +0100

    try again

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const authenticateToken = require('./Routes/authMiddleware');
 
 
 const app = express();
+app.use(cors({credentials:true,
+    methods:['GET','POST'],
+    allowedHeaders: 'Content-Type,Authorization'
+}));
 
 const saveRoutes = require('./Routes/saveRoutes');
 
 const listeningPort = process.env.API_LISTENING_PORT;
 
-app.use(cors({origin : '*',
-    credentials:true,
-    methods:['GET','POST'],
-    allowedHeaders: 'Content-Type,Authorization'
-}));
+
 app.use(express.json({limit:'100mb'}));
 app.use(express.urlencoded({limit:'100mb',extended :true, parameterlimit:1000000}));
 app.use('/api/savemanagement', saveRoutes);
