0d5f981740b17afc95da31cc8f511f5c287f0377
raugdae <raugdae@gmail.com>
2024-12-27 21:02:24 +0100

    another commit

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use(cors({
 app.use(express.json({limit:'100mb'}));
 app.use(express.urlencoded({limit:'100mb',extended :true, parameterlimit:1000000}));
 
- // Additional headers for broader access
+ /*// Additional headers for broader access
  app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', '*');
     next();
- });
+ });*/
 
 
 
