2199f53c41395efdccb3422f5a4d5e9ed44ce419
raugdae <raugdae@gmail.com>
2024-12-29 12:12:54 +0100

    chage on userCOntrooler

--- Routes/userController.js
+++ Routes/userController.js
@@ -1,28 +1,31 @@
 const UserModel = require('./userModel');
 const jwt = require('jsonwebtoken');
 
+async function register(req, res) {
+    try {
+        console.log('registering function entered');
+        const { email, password } = req.body;
 
-
-    async function register(req,res){
-        try{
-            console.log('registering fucntion entered');
-            const {email, password} = req.body;
-
-            if(!email || !password) {
-                return res.status(400).json({message : 'email or password should not be empty'});
-            }
-
-            const user = await UserModel.createUser(email,password);
-
-            const
-             token = jwt.sign({userId:user.id, email:user.email},process.env.JWT_SECRET, {expiresIn : '24h'});
-
-             res.status(201).json({
-                messsge : 'User created'
-             ,token});
-        } catch (error) {
-            return res.status(400).json({message : 'error while creating user'});
+        if (!email || !password) {
+            return res.status(400).json({ message: 'email or password should not be empty' });
         }
-    }
 
-    module.exports = {register};
+        const user = await UserModel.createUser(email, password);
+        const token = jwt.sign(
+            { userId: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        );
+
+        res.status(201).json({
+            message: 'User created',
+            user: { id: user.id, email: user.email },
+            token
+        });
+    } catch (error) {
+        console.error('Registration error:', error);
+        return res.status(500).json({ message: 'Error while creating user' });
+    }
+}
+
+module.exports = { register };  // Export comme objet
