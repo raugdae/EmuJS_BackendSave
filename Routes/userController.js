df47d51bc011e264669804d7a1e7fe09a1e3596a
raugdae <raugdae@gmail.com>
2024-12-29 14:37:53 +0100

    prout

--- Routes/userController.js
+++ Routes/userController.js
@@ -69,11 +69,11 @@ router.post('/login', async (req,res) =>{
     console.log("Entering login function");
     try {
 
-        console.log(req.body);
+        //console.log(req.body);
 
-        const {email,password} = req.body;
+        const { email , password } = req.body;
 
-        FileSystem.log(email, password);
+        console.log(email, password);
 
         if (!email || !password){
             res.status(403).json({message:'enter credentials'});
