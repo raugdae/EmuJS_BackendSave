07604c3e654eb06da99a1ae1f4a334947676b2d8
raugdae <raugdae@gmail.com>
2024-12-29 17:20:41 +0100

    loogz

--- Routes/authMiddleware.js
+++ Routes/authMiddleware.js
@@ -6,15 +6,17 @@ const authMiddleware = (req, res,next) =>{
         console.log('entering middleware');
         const token = req.headers.authorization?.split(' ')[1];
 
-        console.log(token);
         if(!token){
             return res.status(401).json({message:'Authentication required'});
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        console.log(decoded);
+
         req.user=decoded;
-            next();
+
+        next();
         
 
     }catch (error){
