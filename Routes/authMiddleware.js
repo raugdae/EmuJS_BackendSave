0138cad5d8353da79d432bee2cc1fb4828233863
raugdae <raugdae@gmail.com>
2024-12-30 13:16:45 +0100

    401 trapping

--- Routes/authMiddleware.js
+++ Routes/authMiddleware.js
@@ -7,6 +7,7 @@ const authMiddleware = (req, res,next) =>{
         const token = req.headers.authorization?.split(' ')[1];
 
         if(!token){
+            console.log('No Token returning 401');
             return res.status(401).json({message:'Authentication required'});
         }
 
