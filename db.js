61a15fb92ea9040ee28c47ab7e2e9b26e8eee304
raugdae <raugdae@gmail.com>
2024-12-30 09:39:53 +0100

    Adding SSL

--- db.js
+++ db.js
@@ -6,7 +6,11 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database:process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port:5432
+    port:5432,
+
+    ssl: {
+        rejectUnauthorized: false 
+      }
 });
 
 module.exports = pool;
