05b2711160385ade3650eff2917e949df206537a
raugdae <raugdae@gmail.com>
2024-12-30 16:59:19 +0100

    return all saves

--- Routes/userInformation.js
+++ Routes/userInformation.js
@@ -66,7 +66,7 @@ router.get('/usersavelist', authMiddleware , async(req,res) =>{
             return res.status(404).json({message : 'User not found'});
         }
         console.log(result.rows);
-        return res.status(200).json(result.rows[0]);
+        return res.status(200).json(result.rows);
 
 
         
@@ -90,7 +90,7 @@ router.get('/usersavelist', authMiddleware , async(req,res) =>{
                 return res.status(404).json({message : 'User not found'});
             }
             console.log(result.rows);
-            return res.status(200).json(result.rows[0]);
+            return res.status(200).json(result.rows);
     
     
             
