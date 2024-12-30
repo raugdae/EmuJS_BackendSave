bb6d7090abd6afdc5bf84c4d1f1fa7c79fa116f4
raugdae <raugdae@gmail.com>
2024-12-30 15:50:12 +0100

    Chage response 402

--- Routes/userInformation.js
+++ Routes/userInformation.js
@@ -8,7 +8,7 @@ router.get('/userprofile', authMiddleware , async(req,res) =>{
     const userId = req.user.userId;
 
     try {
-        const query = 'SELECT nickname, email, creation_date,profile FROM users WHERE id = $1;'
+        const query = 'SELECT users.nickname, users.email, users.creation_date, users.profile,COUNT(games.id) FROM users LEFT JOIN users.id = games.fk_user WHERE users.id = $1;'
         const value = [userId];
 
         const result = await pool.query(query,value);
@@ -17,7 +17,7 @@ router.get('/userprofile', authMiddleware , async(req,res) =>{
             return res.status(404).json({message : 'User not found'});
         }
         console.log(result.rows[0]);
-        return res.status(201).json(result.rows[0]);
+        return res.status(204).json(result.rows[0]);
 
 
         
