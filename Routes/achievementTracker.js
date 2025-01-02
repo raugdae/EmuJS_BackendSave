863b2bd34a82929267be9bed342c51f5a6583cea
raugdae <raugdae@gmail.com>
2025-01-02 14:16:32 +0100

    logs

--- Routes/achievementTracker.js
+++ Routes/achievementTracker.js
@@ -4,6 +4,7 @@ async function updateAchievement(req,res){
 
     const {userId,gameId,data} = req.body;
 
+    console.log('entering achievement update');
     //Fetching achievement from game
     const selectAchievementQuery = `SELECT id,memorylocation,waitedvalue FROM achievement WHERE fk_gamelist = (SELECT id FROM gamelist WHERE filename LIKE $1)`;
     const selectAchievementValues = [gameId];
