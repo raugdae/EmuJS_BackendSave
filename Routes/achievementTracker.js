c0eb38ede16622db65a3f2bf74d16f1795fd1dc2
raugdae <raugdae@gmail.com>
2025-01-02 16:03:16 +0100

    pool change

--- Routes/achievementTracker.js
+++ Routes/achievementTracker.js
@@ -1,20 +1,22 @@
 const {Pool} = require('pg');
 
-const temppool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port:5432,
-    idleTimeoutMillis:30000,
 
-    ssl: {
-        rejectUnauthorized: false 
-      }
-});
 
 async function updateAchievement({gameid},userId,data){
     console.log('entering achievement update');
+
+    const temppool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database:process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port:5432,
+        idleTimeoutMillis:30000,
+    
+        ssl: {
+            rejectUnauthorized: false 
+          }
+    });
     //const {selectAchievementValues} = gameId;
     try {
     
