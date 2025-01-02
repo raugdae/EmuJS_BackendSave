9324b017e3d740c83604cd2e9cbe8d7d62c6402b
raugdae <raugdae@gmail.com>
2025-01-02 15:12:25 +0100

    update

--- Routes/achievementTracker.js
+++ Routes/achievementTracker.js
@@ -4,11 +4,10 @@ async function updateAchievement(gameId,userId,data){
     console.log('entering achievement update');
     try {
     
-        console.log([gameId]);
     
     //Fetching achievement from game
     const selectAchievementQuery = `SELECT id,memorylocation,waitedvalue FROM achievement WHERE fk_gamelist = $1;`;
-    const selectAchievementValues = {gameId};
+    const {selectAchievementValues} = gameId;
 
     console.log('sending query');
     const achievementList = await pool.query(selectAchievementQuery,selectAchievementValues);
