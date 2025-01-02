a426c08cd778c9133338d09366a954b9454d4a6a
raugdae <raugdae@gmail.com>
2025-01-02 16:28:51 +0100

    aéslkdfjh

--- Routes/saveRoutes.js
+++ Routes/saveRoutes.js
@@ -136,8 +136,8 @@ router.post('/updatesavefile', authMiddleware, async (req, res) =>{
 
         
         const resultGameId = await pool.query(queryGameId,queryGameIdValues);
-        const gameIdDB = resultGameId.rows[0];
-        const {gameId} = gameIdDB;
+        const gameId = resultGameId.rows[0];
+        //const {gameId} = gameIdDB;
         
         console.log('id user');
         const queryUserId = 'SELECT id AS userId from users WHERE id = (SELECT fk_user FROM games WHERE id = $1)';
