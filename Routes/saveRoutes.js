cf261f1fbeed63082793f58b1b8e2914e69bff77
raugdae <raugdae@gmail.com>
2025-01-02 16:11:43 +0100

    changepoolname

--- Routes/saveRoutes.js
+++ Routes/saveRoutes.js
@@ -190,7 +190,7 @@ async function updateAchievement({gameid},userId,data){
     console.log(gameId);
     //Fetching achievement from game
     const selectAchievementQuery = `SELECT id,memorylocation,waitedvalue FROM achievement WHERE fk_gamelist = $1`;
-    const achievementList = await temppool.query(selectAchievementQuery,gameId);
+    const achievementList = await pool.query(selectAchievementQuery,gameId);
 
     console.log('query returned');
     const listJson = achievementList.rows;
