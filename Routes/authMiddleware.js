9fc33bb6531c62d354536dc75f25d90878ea689a
raugdae <raugdae@gmail.com>
2025-01-04 00:54:05 +0100

    remove auth loggin

--- Routes/authMiddleware.js
+++ Routes/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res,next) =>{
     try {
 
-        console.log('entering middleware');
+        //console.log('entering middleware');
         const token = req.headers.authorization?.split(' ')[1];
 
         if(!token){
