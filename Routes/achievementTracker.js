b3f1c6f8f1f6a09f1f09a5d24def8b947f1d689d
raugdae <raugdae@gmail.com>
2025-01-05 21:48:10 +0100

    logs

--- Routes/achievementTracker.js
+++ Routes/achievementTracker.js
@@ -2,7 +2,7 @@ function updateAchievement(achievementList,data){
     //console.log('entering achievement update');
 
     achievementList.forEach(achievement => {
-        console.log (data.rows[achievement.memorylocation]);
+        console.log (data.rows[0]);
         //console.log(achievement);
         if (achievement.achievementcondition === 'binarycheck'){
             //console.log ('Achievement is binary compare');
