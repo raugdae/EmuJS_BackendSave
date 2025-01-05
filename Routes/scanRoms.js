6f336e6e971bcd4697dba3a3cf59448a6c6b9f4b
raugdae <raugdae@gmail.com>
2025-01-05 13:27:46 +0100

    return payload

--- Routes/scanRoms.js
+++ Routes/scanRoms.js
@@ -89,7 +89,7 @@ router.get('/updateromlist', async (req,res) =>{
 
         console.log(payloadJsonified);
 
-        return res.status(200).json({message : 'payload not ready'});
+        return res.status(200).json(payloadJsonified);
 
     }
     catch (err) {
