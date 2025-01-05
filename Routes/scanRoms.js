10af5f5eb99df2ce81481c9231c7eefbd655481b
raugdae <raugdae@gmail.com>
2025-01-05 22:33:51 +0100

    change array

--- Routes/scanRoms.js
+++ Routes/scanRoms.js
@@ -179,7 +179,7 @@ router.post('/updateromdata', authMiddleware, async (req,res) =>{
         console.log(inputData);
         const queryUpdateRomData = `UPDATE gamelist SET (name = $1), (filename = $2), (boxartpath = $3), (yearofdistribution = $4), (developer = $5), (rompath = $6), (categorie = $7)
                                     WHERE filename = $2`;
-        const queryUpdateRomDataValue = [inputData.title,inputData.romPath.split('/')[5],inputData.boxArtPath,inputData.year,inputData.developer,inputData.romPath,inputData.categories];
+        const queryUpdateRomDataValue = [inputData.title,inputData.romPath.split('/')[3],inputData.boxArtPath,inputData.year,inputData.developer,inputData.romPath,inputData.categories];
 
         const resultUpdateRomData = pool.query(queryUpdateRomData,queryUpdateRomDataValue);
 
